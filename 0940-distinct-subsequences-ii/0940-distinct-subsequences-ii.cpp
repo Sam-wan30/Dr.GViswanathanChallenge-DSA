c2d685535cca6b4f0c6397e2dab5f13ae74b496f
Samiksha V. Wanjari <samikshawanjari30@gmail.com>
2026-09-07 22:25:18 +0530

    Time: 0 ms (100%), Space: 9.9 MB (22.94%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const int MOD = 1e9 + 7;
+        int n = s.size();
+
+        vector<long long> dp(n + 1);
+        vector<int> last(26, -1);
+
+        dp[0] = 1;  // empty subsequence
+
+        for (int i = 1; i <= n; i++) {
+
+            // Take or don't take s[i-1]
+            dp[i] = (2 * dp[i - 1]) % MOD;
+
+            int ch = s[i - 1] - 'a';
+
+            // Remove duplicates
+            if (last[ch] != -1) {
+                dp[i] = (dp[i] - dp[last[ch]] + MOD) % MOD;
+            }
+
+            last[ch] = i - 1;
+        }
+
+        // Remove empty subsequence
+        return (dp[n] - 1 + MOD) % MOD;
+    }
+};
